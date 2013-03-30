390bc5b6dd5ddbb74a481da13fc36d06cda166c2
Brian T Bailey <briantbailey@gmail.com>
2013-03-29 19:51:09 -0500

    Added basic CrimeRecord Class

---
+++ bbailey4_iOSFinal/CrimeRecord.h
@@ -0,0 +1,29 @@
+//
+//  CrimeRecord.h
+//  bbailey4_iOSFinal
+//
+//  Created by Brian Bailey on 3/29/13.
+//  Copyright (c) 2013 Brian Bailey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CrimeRecord : NSObject
+
+@property (copy, nonatomic) NSString *caseNum;
+@property (copy, nonatomic) NSString *dateOf;
+@property (copy, nonatomic) NSString *block;
+@property (copy, nonatomic) NSString *iucr;
+@property (copy, nonatomic) NSString *primaryDesc;
+@property (copy, nonatomic) NSString *secondaryDesc;
+@property (copy, nonatomic) NSString *locationDesc;
+@property (copy, nonatomic) NSString *arrest;
+@property (copy, nonatomic) NSString *domestic;
+@property (copy, nonatomic) NSString *beat;
+@property (copy, nonatomic) NSString *ward;
+@property (copy, nonatomic) NSString *fbi_cd;
+@property (strong, nonatomic) NSNumber *latitude;
+@property (strong, nonatomic) NSNumber *longitude;
+@property (strong, nonatomic) NSNumber *distanceToMyLocation;
+
+@end
---
+++ bbailey4_iOSFinal/CrimeRecord.m
@@ -0,0 +1,33 @@
+//
+//  CrimeRecord.m
+//  bbailey4_iOSFinal
+//
+//  Created by Brian Bailey on 3/29/13.
+//  Copyright (c) 2013 Brian Bailey. All rights reserved.
+//
+
+#import "CrimeRecord.h"
+
+@interface CrimeRecord()
+
+@end
+
+@implementation CrimeRecord
+
+@synthesize caseNum = _caseNum;
+@synthesize dateOf = _dateOf;
+@synthesize block = _block;
+@synthesize iucr = _iucr;
+@synthesize primaryDesc = _primaryDesc;
+@synthesize secondaryDesc = _secondaryDesc;
+@synthesize locationDesc = _locationDesc;
+@synthesize arrest = _arrest;
+@synthesize domestic = _domestic;
+@synthesize beat = _beat;
+@synthesize ward = _ward;
+@synthesize fbi_cd = _fbi_cd;
+@synthesize latitude = _latitude;
+@synthesize longitude = _longitude;
+@synthesize distanceToMyLocation = _distanceToMyLocation;
+
+@end
